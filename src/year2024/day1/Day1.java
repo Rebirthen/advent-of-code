4bcc9161b3763e8a226d190d6520b55b288d81ea
Diana <rebirthed.flame@gmail.com>
2024-12-01 16:04:49 +0100

    advent of code 1 day

--- src/year2024/day1/Day1.java
+++ src/year2024/day1/Day1.java
@@ -11,17 +11,15 @@ public class Day1 {
 
         int sum = calculateSimilarityScore(lines);
 
-        System.out.println("sum:"+ sum);
+        System.out.println("sum:" + sum);
     }
 
 
-
-
-    public static int calculateDistance(List<String> lines){
+    public static int calculateDistance(List<String> lines) {
         int res = 0;
         List<Integer> place1 = new ArrayList<>();
         List<Integer> place2 = new ArrayList<>();
-        for(String line : lines){
+        for (String line : lines) {
             int[] numbers = Arrays.stream(line.split("\\s+")).mapToInt(Integer::parseInt).toArray();
             place1.add(numbers[0]);
             place2.add(numbers[1]);
@@ -29,31 +27,28 @@ public class Day1 {
         place1.sort(Integer::compareTo);
         place2.sort(Integer::compareTo);
 
-        while(!place1.isEmpty())
-        {
-            res+=Math.abs(place1.remove(0) - place2.remove(0));
+        while (!place1.isEmpty()) {
+            res += Math.abs(place1.remove(0) - place2.remove(0));
         }
         return res;
     }
 
-    public static int calculateSimilarityScore(List<String> lines){
+    public static int calculateSimilarityScore(List<String> lines) {
         int res = 0;
         List<Integer> leftList = new ArrayList<>();
         Map<Integer, Integer> rightListToCount = new HashMap<>();
-        for(String line : lines){
+        for (String line : lines) {
             int[] numbers = Arrays.stream(line.split("\\s+")).mapToInt(Integer::parseInt).toArray();
             leftList.add(numbers[0]);
             rightListToCount.put(numbers[1], rightListToCount.getOrDefault(numbers[1], 0) + 1);
         }
 
-        while(!leftList.isEmpty())
-        {
+        while (!leftList.isEmpty()) {
             int num = leftList.remove(0);
-            res+=Math.abs(num * rightListToCount.getOrDefault(num, 0));
+            res += Math.abs(num * rightListToCount.getOrDefault(num, 0));
         }
         return res;
     }
-
 
 
 }
